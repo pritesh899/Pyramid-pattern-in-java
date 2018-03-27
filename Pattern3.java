4e0204818aed3efbbb21b54466de31d2bd9e13c3
Pritesh Ratnappagol <pritesh.ratnappagol@gmail.com>
2018-03-26 23:51:16 -0400

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,33 @@
+//			      *
+//			     **
+//			    ***
+//			   ****
+//			  *****
+//			 ******
+//			*******
+
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		Pattern(6);
+	}
+	public static void Pattern(int n)
+	{
+		int i , j;
+		for(i = 0 ; i <= n; i ++)
+		{
+			for(j = 0; j<= n;j++)
+			{
+				if(j <= n && j >= n -i)
+				{
+					System.out.print("*");
+				}else{
+					System.out.print(" ");
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+
+}
